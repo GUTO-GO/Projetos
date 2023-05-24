b6aa891a2f12aa019352ea4c0a6bc506dff5584a
GUTO-GO <gutogo551104@gmail.com>
2023-05-24 15:34:39 -0300

    Lista1

---
+++ LISTA1/Ex05.java
@@ -0,0 +1,15 @@
+// 5 – Crie um arquivo chamado TrianguloComBarraN.java. Implemente um programa que exiba na
+// saída padrão um triângulo de asteriscos semelhante ao mostrado a seguir. Utilize a sequência de
+// escape \n para desenvolver esse programa.
+// *
+// **
+// ***
+// ****
+// *****
+
+public class Ex05 {
+	public static void main (String[] args) {
+		System.out.print("*\n**\n***\n****\n*****");
+	}
+}
+//Marcos Augusto Carvalho da Silva
---
+++ LISTA1/Ex09.java
@@ -0,0 +1,18 @@
+// 9 - Crie um arquivo chamado Fatec.java. Implemente um programa que mostre a seguinte saída.
+// Compile e execute esse programa.
+// #####  ###  ##### #####  ###  
+// #     #   #   #   #     #   # 
+// ####  #####   #   ####  #     
+// #     #   #   #   #     #   # 
+// #     #   #   #   #####  ###  
+
+public class Ex09 {
+	public static void main (String[] args) {
+		System.out.print("#####  ###  ##### #####  ###  \n");
+		System.out.print("#     #   #   #   #     #   # \n");
+		System.out.print("####  #####   #   ####  #     \n");
+		System.out.print("#     #   #   #   #     #   # \n");
+		System.out.print("#     #   #   #   #####  ###  ");
+	}
+}
+//Marcos Augusto Carvalho da Silva
