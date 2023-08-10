1060093274e0a943839b183f16667b5206ce1e84
GUTO-GO <gutogo551104@gmail.com>
2023-08-10 00:21:02 -0300

    Lista3

---
+++ Linguagem de Programação - JAVA - 2ºSemestre ADS/LISTA 3 - Operadores/Ex07.java
@@ -0,0 +1,19 @@
+// 7 - Crie um arquivo chamado OverflowUnderflow.java. Complete o código, substituindo a
+// seqüência “xxxxx” com valores literais. O programa deve exibir os números -2147483648 e
+// 2147483647 nessa ordem.
+
+// public class OverflowUnderflow {
+//  public static void main(String[] args) {
+//   System.out.println( xxxxx + 1);
+//   System.out.println( xxxxx - 1);
+//  }
+// }
+
+
+public class Ex07 {
+	public static void main (String[] args) {
+		System.out.println( 2147483647 + 1);
+ 		System.out.println( -2147483648 - 1);
+	}
+}
+//Marcos Augusto Carvalho da Silva
