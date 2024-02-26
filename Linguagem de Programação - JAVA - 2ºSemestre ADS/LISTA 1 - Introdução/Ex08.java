7aad157ba97b42bf99ea9c76d4fb675f95258fdb
GUTO-GO <gutogo551104@gmail.com>
2024-02-26 07:16:06 -0300

    0

--- Linguagem de Programação - JAVA - 2ºSemestre ADS/LISTA 1 - Introdução/Ex08.java
+++
@@ -1,9 +0,0 @@
-// 8 - Crie um arquivo chamado FrasePreferida.java. Implemente um programa que mostre na saída
-// padrão a sua frase preferida. Compile e execute esse programa.
-
-public class Ex08 {	
-	public static void main (String[] args) {
-		System.out.println("A vinganca nunca e plena, mata a alma e envenena.");
-	}
-}
-//Marcos Augusto Carvalho da Silva
